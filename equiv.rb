2f8b9def9629deb96981ebb7d4b0bc0e5b260325
Fatih Kadir Akın <f@users.noreply.github.com>
2016-01-10 23:40:32 +0200

    Add libpcl and bdw-gc formula dependencies

--- equiv.rb
+++ equiv.rb
@@ -4,6 +4,9 @@ class Equiv < Formula
   url "https://github.com/f/equiv/archive/0.0.1.tar.gz"
   version "0.0.1"
   sha256 "5bd01cedb7be5518f4f847280cf74a1cfe54ff8a0a1cc08ea93864628137b51d"
+  
+  depends_on "libpcl"
+  depends_on "bdw-gc"
 
   def install
     bin.install "equiv"
