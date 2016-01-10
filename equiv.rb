b922740549f21cc0935d1ddceaec4b01b3a24c8f
Fatih Kadir Akın <fakin@protel.com.tr>
2016-01-10 17:06:22 +0200

    Initial Commit

---
+++ equiv.rb
@@ -0,0 +1,11 @@
+class Guardian < Formula
+  desc "Inter-Language Equivalent Package Finder"
+  homepage "http://github.com/f/equiv"
+  url "https://github.com/f/equiv/archive/v0.0.1.tar.gz"
+  version "0.0.1"
+  sha256 ""
+
+  def install
+    bin.install "equiv"
+  end
+end
