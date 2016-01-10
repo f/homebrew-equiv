4918df53e9f4f416f95bc9f6653513e400d58c1d
Fatih Kadir Akın <f@users.noreply.github.com>
2016-01-10 17:08:16 +0200

    Update SHA

--- equiv.rb
+++ equiv.rb
@@ -1,9 +1,9 @@
 class Guardian < Formula
   desc "Inter-Language Equivalent Package Finder"
   homepage "http://github.com/f/equiv"
-  url "https://github.com/f/equiv/archive/v0.0.1.tar.gz"
+  url "https://github.com/f/equiv/archive/0.0.1.tar.gz"
   version "0.0.1"
-  sha256 ""
+  sha256 "5bd01cedb7be5518f4f847280cf74a1cfe54ff8a0a1cc08ea93864628137b51d"
 
   def install
     bin.install "equiv"
